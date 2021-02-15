7b45d7e84b39ae5dae1de12d5cd8f8585c1cb69d
Jakub Szewczyk <git@kubasz.xyz>
2021-02-15 15:59:31 +0000

    Initial project structure

---
+++ include/wndpe/wndpe.h
@@ -0,0 +1,6 @@
+#ifndef WNDPE_H_INCLUDED
+#define WNDPE_H_INCLUDED 1
+
+
+
+#endif
---
+++ src/driver/main.cpp
@@ -0,0 +1,9 @@
+#include "wndpe/wndpe.h"
+#include <iostream>
+
+using std::cout, std::cerr, std::endl;
+
+int main(int argc, char** argv) {
+    cout << "Wasm NDP Extractor CLI" << endl;
+    return 0;
+}
